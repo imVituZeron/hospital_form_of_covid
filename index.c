62a3065d6e8eee2fafe6968321ceb793c0b8598b
Vitor de Paula Santos <vitorpaulasantos@gmail.com>
2022-10-29 23:20:19 -0300

    I created the structs and continued login option

--- index.c
+++ index.c
@@ -1,21 +1,77 @@
 #include <stdio.h>
 
+    struct agente{
+        char nome[30];
+        char usuario[40];
+        char senha[10];
+    };
+
+    struct paciente{
+        char nome[30];
+        char email[40];
+        int cpf[11];
+        int telefone[11];
+        char comorbidade[40];
+        int data_nasc[2];
+        int data_diag[2];
+    };
+
+    struct endereco{
+        char rua[30];
+        char bairro[25];
+        char cidade[25];
+        char estado[2];
+        int numero[4];
+        int cep[8];
+    };
+
+
+
+
 int main(void){
-    int opcao;
+
+    struct agente agente;
+    int opcao, opcao_d_login;
 
     printf("-------------\n");
     printf(" Login - 1\n");
-    printf(" Cadastro - 2\n");
+    printf(" Sair - 2\n");
     printf("-------------\n");
     printf("Digite sua opção: ");
     scanf("%i", &opcao);
 
-    if(opcao == 1){
-        printf("opção == 1");
-    }else if(opcao == 2){
-        printf("opção == 2");
+    while(true){
+        if(opcao == 1){
+            printf("--- LOGIN ---\n");
+            printf("Usuário: ");
+            scanf("%s", agente.usuario);
+            printf("Senha: ");
+            scanf("%s", agente.senha);
+            printf("%s ; %s\n", agente.usuario, agente.senha);
+
+            printf("-------------\n");
+            printf(" Cadastrar paciente - 1\n");
+            printf(" Sair - 2\n");
+            printf("-------------\n");
+            printf("Digite sua opção: ");
+            scanf("%i", &opcao_d_login);
+
+            if(opcao_d_login == 2){
+                opcao = 2;
+            }else if(opcao_d_login == 1){
+                printf("ESTÁ AQUI!");
+                break;
+            }
+            
+            
+        }else if(opcao == 2){
+            printf("Até a proxima!\n");
+            break;
+        }else{
+            printf("Informação invalida!\n");
+            break;
+        }
     }
-
-
-
+    
+    return 0;
 }

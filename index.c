65d099c7dfd221c733e4079b331b796f1bd1254e
Vitor de Paula Santos <vitorpaulasantos@gmail.com>
2022-10-30 00:28:36 -0300

    Was initialized grupo_de_risco function construction but i've more problems with strings again

--- index.c
+++ index.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+    struct endereco{
+        char rua[30];
+        char bairro[25];
+        char cidade[25];
+        char estado[2];
+        int numero[4];
+        int cep[8];
+    };
+
     struct agente{
         char nome[30];
         char usuario[40];
@@ -12,24 +21,17 @@
         int cpf[11];
         int telefone[11];
         char comorbidade[40];
-        int data_nasc[2];
+        char data_nasc[2];
         int data_diag[2];
+        struct endereco endereco_paciente;
     };
 
-    struct endereco{
-        char rua[30];
-        char bairro[25];
-        char cidade[25];
-        char estado[2];
-        int numero[4];
-        int cep[8];
-    };
-
-
-
+    // prototipando as funções com estruturas
+    struct paciente cria_arquivo_paciente(struct paciente value);
+    struct paciente cria_arquivo_grupo_de_risco(struct paciente value);
 
 int main(void){
-
+    struct paciente paciente;
     struct agente agente;
     int opcao, opcao_d_login;
 
@@ -56,9 +58,45 @@ int main(void){
             printf("Digite sua opção: ");
             scanf("%i", &opcao_d_login);
 
-            if(opcao_d_login == 2){
-                opcao = 2;
-            }else if(opcao_d_login == 1){
+            if(opcao_d_login == 1){
+                printf("--- CADASTRO DO PACIENTE ---\n");
+                printf("Nome: ");
+                scanf("%s", paciente.nome);
+                printf("Email: ");
+                scanf("%s", paciente.email);
+                printf("CPF: ");
+                scanf("%i", paciente.cpf);
+                printf("Telefone: ");
+                scanf("%i", paciente.telefone);
+                printf("comorbidade: ");
+                scanf("%s", paciente.comorbidade);
+                printf(">--- Data Nascimento ---<\n");
+                printf("Dia: ");
+                scanf("%s", &paciente.data_nasc[0]);
+                setbuf(stdin, NULL);
+                printf("Mês: ");
+                scanf("%s", &paciente.data_nasc[1]);
+                setbuf(stdin, NULL);
+                printf("Ano: ");
+                scanf("%s", &paciente.data_nasc[2]);
+                setbuf(stdin, NULL);
+                printf("--------------------------\n");
+                printf(">--- Data Diagnostico ---<\n");
+                printf("Dia: ");
+                scanf("%i", &paciente.data_diag[0]);
+                setbuf(stdin, NULL);
+                printf("Mês: ");
+                scanf("%i", &paciente.data_diag[1]);
+                setbuf(stdin, NULL);
+                printf("Ano: ");
+                scanf("%i", &paciente.data_diag[2]);
+                setbuf(stdin, NULL);
+                printf("--------------------------\n");
+                cria_arquivo_paciente(paciente);
+                cria_arquivo_grupo_de_risco(paciente);
+                
+                break;
+            }else if(opcao_d_login == 2){
                 printf("ESTÁ AQUI!");
                 break;
             }
@@ -74,4 +112,24 @@ int main(void){
     }
     
     return 0;
+}
+
+struct paciente cria_arquivo_paciente(struct paciente value){
+    printf("prototipo - cadastro paciente\n");
+
+    return value;
+};
+
+struct paciente cria_arquivo_grupo_de_risco(struct paciente value){
+    int idade = 2022 - (int)value.data_nasc[2]; // esta com alguns erros
+    printf("nasc - %i\n", (int)value.data_nasc[2]); // esta com alguns erros
+    printf("%s\n", value.comorbidade);
+
+    if(value.comorbidade != "\0" && idade >= 65){
+        printf("vai ser criado o arquivo deste paciente por ser do grupo de risco!\n");
+    }else{
+        printf("saindo da função grupo_de_risco\n");
+    }
+
+    return value;
 }

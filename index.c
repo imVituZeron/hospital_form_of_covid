0184334c32d0e33d8a5574d4eabdac29d1ae695b
Vitor de Paula Santos <vitorpaulasantos@gmail.com>
2022-11-23 00:22:53 -0300

    Was made casting function and i changed some types of variables

--- index.c
+++ index.c
@@ -13,8 +13,8 @@
     };
 
     struct datas{
-        unsigned int dia;
-        unsigned int mes;
+        char dia[2];
+        char mes[2];
         int ano;
     };
 
@@ -27,8 +27,8 @@
     struct paciente{
         char nome[256];
         char email[256];
-        unsigned int cpf;
-        int telefone[11];
+        char cpf[11];
+        char telefone[11];
         char comorbidade[256];
         struct datas data_nasc;
         struct datas data_diag;
@@ -40,6 +40,7 @@
     int verificar_agente(struct agente value);
     void __fpurge(FILE *stream);
     void pegar_entrada(char *buff);
+    int cast_array_para_int(char *buff, char letra);
     
 int main(void){
     struct paciente paciente;
@@ -83,23 +84,23 @@ int main(void){
                 printf("Email: ");
                 pegar_entrada(paciente.email);
                 printf("CPF: ");
-                scanf("%i", &paciente.cpf);
+                pegar_entrada(paciente.cpf);
                 printf("Telefone: ");
-                scanf("%i", paciente.telefone);
+                pegar_entrada(paciente.telefone);
                 printf("comorbidade: ");
                 pegar_entrada(paciente.comorbidade);
                 printf(">--- Data Nascimento ---<\n");
                 printf("Dia: ");
-                scanf("%i", &paciente.data_nasc.dia);
+                pegar_entrada(paciente.data_nasc.dia);
                 printf("Mês: ");
-                scanf("%i", &paciente.data_nasc.mes);
+                pegar_entrada(paciente.data_nasc.mes);
                 printf("Ano: ");
                 scanf("%i", &paciente.data_nasc.ano);
                 printf(">--- Data Diagnostico ---<\n");
                 printf("Dia: ");
-                scanf("%i", &paciente.data_diag.dia);
+                pegar_entrada(paciente.data_diag.dia);
                 printf("Mês: ");
-                scanf("%i", &paciente.data_diag.mes);
+                pegar_entrada(paciente.data_diag.mes);;
                 printf("Ano: ");
                 scanf("%i", &paciente.data_diag.ano);
                 printf(">--- Endereço ---<\n");
@@ -116,9 +117,6 @@ int main(void){
                 printf("CEP: ");
                 scanf("%i", &paciente.endereco_paciente.cep);
 
-                cria_arquivo_paciente(paciente);
-                cria_arquivo_grupo_de_risco(paciente);
-
                 system("clear");
                 printf("Deseja Cadastrar mais alguma paciente?\n");
                 printf("---------------\n");
@@ -129,10 +127,12 @@ int main(void){
                 scanf("%i", &mais_um);
 
                 if(mais_um == 1){
-                    printf("Paciente anterior cadastrado com sucesso!\n");
+                    cria_arquivo_paciente(paciente);
+                    cria_arquivo_grupo_de_risco(paciente);
                     opcao_d_login = 1;
                 }else if(mais_um == 0){
-                    printf("Paciente cadastrado com sucesso!\n");
+                    cria_arquivo_paciente(paciente);
+                    cria_arquivo_grupo_de_risco(paciente);
                     opcao_d_login = 2;
                     break;
                 }
@@ -168,11 +168,11 @@ struct paciente cria_arquivo_paciente(struct paciente value){
     file = fopen(nome_do_arquivo, "w");
         fprintf(file,"Nome: %s\n", value.nome);
         fprintf(file,"Email: %s\n", value.email);
-        fprintf(file,"Cpf: %i\n", value.cpf);
-        fprintf(file,"Telefone: %n\n", value.telefone);
+        fprintf(file,"Cpf: %s\n", value.cpf);
+        fprintf(file,"Telefone: %s\n", value.telefone);
         fprintf(file,"Comorbidade: %s\n", value.comorbidade);
-        fprintf(file,"Data de Nasc: %i/%i/%i\n", value.data_nasc.dia, value.data_nasc.mes, value.data_nasc.ano);
-        fprintf(file,"Data de Diag: %i/%i/%i\n", value.data_diag.dia, value.data_diag.mes, value.data_diag.ano);
+        fprintf(file,"Data de Nasc: %i/%i/%i\n", cast_array_para_int(value.data_nasc.dia, 'd'), cast_array_para_int(value.data_nasc.mes, 'm'), value.data_nasc.ano);
+        fprintf(file,"Data de Diag: %i/%i/%i\n", cast_array_para_int(value.data_diag.dia, 'd'), cast_array_para_int(value.data_diag.mes, 'm'), value.data_diag.ano);
         fprintf(file,"Rua/Número/Bairro: %s - nº%i - %s\n", value.endereco_paciente.rua, value.endereco_paciente.numero, value.endereco_paciente.bairro);
         fprintf(file,"cidade/Estado: %s - %s\n", value.endereco_paciente.cidade, value.endereco_paciente.estado);
         fprintf(file,"CEP: %i\n", value.endereco_paciente.cep);
@@ -201,7 +201,7 @@ struct paciente cria_arquivo_grupo_de_risco(struct paciente value){
 
         file = fopen(nome_do_arquivo,"w");
             fprintf(file, "Nome: %s\n",value.nome);
-            fprintf(file, "CPF: %d\n",value.cpf);
+            fprintf(file, "CPF: %s\n",value.cpf);
             fprintf(file, "CEP: %i\n",value.endereco_paciente.cep);
             fprintf(file, "Comorbidade: %s\n",value.comorbidade);
         fclose(file);
@@ -257,4 +257,17 @@ void pegar_entrada(char *buff){
     len = strlen(buff);
     if(buff[len - 1] == '\n')
         buff[--len] = '\0';
+};
+
+int cast_array_para_int(char *buff, char letra){
+    int dia, mes;
+
+    if(letra == 'd'){
+        sscanf(buff, "%d", &dia);
+        return dia;
+    }else if(letra == 'm'){
+        sscanf(buff, "%d", &mes);
+        return mes;
+    }
+    return 0;
 };
